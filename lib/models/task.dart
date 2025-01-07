8064c339ea97a9f735dd304ff4b964c3b8a63436
paudelgamer <paudeladrin@github.com>
2025-01-07 14:51:15 +0545

    first commit

---
+++ lib/models/task.dart
@@ -0,0 +1,33 @@
+class Task {
+  String title;
+  DateTime? date;
+  TimeOfDay? time;
+  bool notification;
+  String group; // Work, College, Home, Personal
+  String importance; // High, Medium, Low, Not Specified
+  String? details;
+  List<SubPoint>? subPoints; // List of sub-tasks
+  bool isCompleted;
+
+  Task({
+    required this.title,
+    this.date,
+    this.time,
+    this.notification = false,
+    this.group = 'Personal',
+    this.importance = 'Not Specified',
+    this.details,
+    this.subPoints,
+    this.isCompleted = false,
+  });
+}
+
+class SubPoint {
+  String title;
+  bool isCompleted;
+
+  SubPoint({
+    required this.title,
+    this.isCompleted = false,
+  });
+}
